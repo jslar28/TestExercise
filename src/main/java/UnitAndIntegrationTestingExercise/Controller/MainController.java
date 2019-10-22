cbb01cd1a399d1c36d79403a87866a035e39be6a
joachim104 <joachim.stougaard@hotmail.com>
2019-10-22 13:55:17 +0200

    commit

--- src/main/java/UnitAndIntegrationTestingExercise/Controller/MainController.java
+++ src/main/java/UnitAndIntegrationTestingExercise/Controller/MainController.java
@@ -4,18 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+
+    public static int totalAmount = 0;
 
     @RequestMapping(value = {"", "/", "/index"}, method = RequestMethod.GET)
     public String index() {
         return "index.html";
     }
 
-    @GetMapping("/lol")
-    public String lol() {
-        System.out.println("lol");
-        return "index.html";
+
+
+    @GetMapping("/internet-connection")
+    @ResponseBody
+    public boolean internetConnection(boolean internetConnection) {
+        if (internetConnection) {
+            return true;
+        }
+        else return false;
     }
 }
