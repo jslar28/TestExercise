8ca4d5a1d881820b32a7453a6996ad068fdfac78
Jasper Hessellund <jasperhessellund@gmail.com>
2019-10-29 11:09:33 +0100

    Finished tests

--- src/main/java/UnitAndIntegrationTestingExercise/Controller/Purchase.java
+++ src/main/java/UnitAndIntegrationTestingExercise/Controller/Purchase.java
@@ -59,23 +59,23 @@ public class Purchase {
         switch(modelName) {
             case "Motorola G99":
                 System.out.println("moto11");
-                sum = sum + 800;
+                sum = sum - 800;
                 break;
             case "iPhone 99":
                 System.out.println("ihpone11");
-                sum = sum + 6000;
+                sum = sum - 6000;
                 break;
             case "Samsung Galaxy 99":
                 System.out.println("samsung11");
-                sum = sum + 1000;
+                sum = sum - 1000;
                 break;
             case "Sony Xperia 99":
                 System.out.println("sony11");
-                sum = sum + 900;
+                sum = sum - 900;
                 break;
             case "Huawei 99":
                 System.out.println("huawei11");
-                sum = sum + 900;
+                sum = sum - 900;
                 break;
             default:
                 System.out.println("default!");
--- src/test/java/UnitAndIntegrationTestingExercise/Controller/PurchaseTest.java
+++ src/test/java/UnitAndIntegrationTestingExercise/Controller/PurchaseTest.java
@@ -17,18 +17,54 @@ class PurchaseTest {
     void phoneLinesIncrement() {
         int expected = 150;
         int actual = 140;
-        assertEquals(expected, actual);
+        assertEquals(expected, actual, "Failed on purpose");
     }
 
     @Test
     void phoneLinesDecrement() {
+        int expected = -150;
+        int actual = -150;
+        assertEquals(expected, actual);
     }
 
     @Test
     void addPhone() {
+        Purchase purchase = new Purchase();
+        assertEquals(purchase.addPhone("Motorola G99"), 800);
+        purchase.sum = 0;
+        assertEquals(purchase.addPhone("iPhone 99"), 6000);
+        purchase.sum = 0;
+
+        assertEquals(purchase.addPhone("Samsung Galaxy 99"), 1000);
+        purchase.sum = 0;
+
+        assertEquals(purchase.addPhone("Sony Xperia 99"), 900);
+        purchase.sum = 0;
+
+        assertEquals(purchase.addPhone("Huawei 99"), 900);
+        purchase.sum = 0;
+        assertEquals(purchase.addPhone("Nokia 99"), 0);
+        purchase.sum = 0;
+
     }
 
     @Test
     void removePhone() {
+        Purchase purchase = new Purchase();
+        assertEquals(purchase.removePhone("Motorola G99"), -800);
+        purchase.sum = 0;
+        assertEquals(purchase.removePhone("iPhone 99"), -6000);
+        purchase.sum = 0;
+
+        assertEquals(purchase.removePhone("Samsung Galaxy 99"), -1000);
+        purchase.sum = 0;
+
+        assertEquals(purchase.removePhone("Sony Xperia 99"), -900);
+        purchase.sum = 0;
+
+        assertEquals(purchase.removePhone("Huawei 99"), -900);
+        purchase.sum = 0;
+        assertEquals(purchase.removePhone("Nokia 99"), 0);
+        purchase.sum = 0;
     }
 }
